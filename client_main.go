3f329c2abdaf71af4292f73f7767308dd8ee8fe7
BHAVANI514 <bhavanikarnati298@gmail.com>
2025-07-21 15:01:30 +0530

    client_server

--- client_main.go
+++ client_main.go
@@ -2,31 +2,31 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-    // Make GET request to server's /ping endpoint
-    resp, err := http.Get("http://localhost:8081/ping")
-    if err != nil {
-        fmt.Println("Error calling server:", err)
-        return
-    }
-    defer resp.Body.Close()
+	// Make GET request to server's /ping endpoint
+	resp, err := http.Get("http://localhost:8081/ping")
+	if err != nil {
+		fmt.Println("Error calling server:", err)
+		return
+	}
+	defer resp.Body.Close()
 
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println("Response from server:", string(body))
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("Response from server:", string(body))
 
-    // Call /greet with query parameter
-    greetResp, err := http.Get("http://localhost:8081/greet?name=Ranjith")
-    if err != nil {
-        fmt.Println("Error calling greet:", err)
-        return
-    }
-    defer greetResp.Body.Close()
+	// Call /greet with query parameter
+	greetResp, err := http.Get("http://localhost:8081/greet?name=Ranjith")
+	if err != nil {
+		fmt.Println("Error calling greet:", err)
+		return
+	}
+	defer greetResp.Body.Close()
 
-    greetBody, _ := ioutil.ReadAll(greetResp.Body)
-    fmt.Println("Greeting:", string(greetBody))
+	greetBody, _ := ioutil.ReadAll(greetResp.Body)
+	fmt.Println("Greeting:", string(greetBody))
 }
--- main.go
+++ main.go
@@ -2,28 +2,29 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 
-    router.GET("/ping", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "pong from server",
-        })
-    })
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong from server",
+		})
+	})
 
-    router.GET("/greet", func(c *gin.Context) {
-        name := c.Query("name")
-        if name == "" {
-            name = "Guest"
-        }
-        c.JSON(http.StatusOK, gin.H{
-            "greeting": "Hello " + name,
-        })
-    })
+	router.GET("/greet", func(c *gin.Context) {
+		name := c.Query("name")
+		if name == "" {
+			name = "Guest"
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"greeting": "Hello " + name,
+		})
+	})
 
-    router.Run(":8081") // start server on port 8081
+	router.Run(":8081") // start server on port 8081
 }
